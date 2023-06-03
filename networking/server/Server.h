24d4ad34473aedcf2076c623e4bf174ab167f83a
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-06-03 23:59:05 +1000

    Added the server framework header
    Added the header for the server - main implementation is being committed tomorrow :)

---
+++ networking/server/Server.h
@@ -0,0 +1,45 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+
+class Server {
+private:
+    bool running;
+
+    sockaddr_in hint;
+    int hintSize;
+
+    int listeningSocket;
+    std::vector<int> clientSockets;
+
+    fd_set currentSockets, readySockets;
+    int setSize = FD_SETSIZE + 1;
+
+    char buffer[4096];
+
+    bool createSocket(int port);
+    bool acceptConnection();
+
+    bool handleConnection(int clientSocket);     // Modify this function to achieve your desired outcomes!
+
+public:
+    Server();
+    ~Server();
+
+    std::string errorLog;   // If anything fails, this will automatically be set to the relevant error message
+
+    bool run(int port);
+    void stop();
+};
+
+#endif
