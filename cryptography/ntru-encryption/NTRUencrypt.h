6af7e6b783bc4c87a353bd5371fd036d61a48449
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-06-11 16:59:15 +1000

    Finalized NTRUencrypt header
    Finalized the header file for NTRUencrypt -> the implementation should come soon (next commit will require some optimizations :)

--- cryptography/ntru-encryption/NTRUencrypt.h
+++ cryptography/ntru-encryption/NTRUencrypt.h
@@ -30,23 +30,23 @@ private:
     void scalarMultiplication(int *p, int s, int *resultPolynomial);                  // Multiply p by s: f(x) * s, where 's' is a constant
     void inversePolynomial(int *p, int mod, char modPower, int *resultPolynomial);    // Inverse Polynomial of p: x^k - x^(k-1) + ... + x^2 + 1 -> {1, 0, 1, ..., -1, 1}
 
-    // Generators
-    void fPolynomialGenerator(int df, int *resultPolynomial);                             // f Polynomial Generator
-    void gPolynomialGenerator(int dg, int *resultPolynomial);                             // g and r polynomial generator
-    void publicKeyGenerator(int *fPolynomial, int *gPolynomial, int *resultPolynomial);   // h open key generation
-    
     // Conversions
     void charToArray(char c, int *array);
     char arrayToChar(int *array);
     void stringToArray(std::string message, int *array);
     std::string arrayToString(int *array, int size);
-    void decimalToHex(int *decimal, std::string *hex);  // Called by: hexToDecimal(&hex, &decimal);
+    void decimalToHex(int *decimal, std::string *hex);      // Called using: hexToDecimal(&hex, &decimal);
     void hexToDecimal(std::string *hex, int *decimal);
+    std::string decodeString(std::string *key);
 
-    // Encryption + Decryption
-    void encryption(int *mPolynomial, int *hPolynomial, int *resultPolynomial);                        // Encryption, m = secret polynomial, h = public polynomial
-    void encryption(int *mPolynomial, int *fPolynomial, int *gPolynomial, int *resultPolynomial);      // Encrypt when the public polynomial isnt known
-    void decryption (int *cipherPolynomial, int *fPolynomial, int *fpPolynomial, int *resultPolynomial);
+    // Generators: f & g Polynomial make up the private key, and the h Polynomial is the public key
+    void fPolynomialGenerator(int df, int *resultPolynomial);                             // f Polynomial Generator - First half of a private key
+    void gPolynomialGenerator(int dg, int *resultPolynomial);                             // g and r polynomial generator - Second half of a private key (r is the random polynomial)
+    void publicKeyGenerator(int *fPolynomial, int *gPolynomial, int *resultPolynomial);   // h Polynomial Generator - The public key
+
+    // Encryption + Decryption: m Polynomial is the plaintext message represented in Polynomial form, fpPolynomial is the modular inverse of fPolynomial modulo 'p'
+    void encryption(int *mPolynomial, int *hPolynomial, int *resultPolynomial);                             // Encryption h = public polynomial
+    void decryption (int *cipherPolynomial, int *fPolynomial, int *fpPolynomial, int *resultPolynomial);    // Decryption: Cypher = Encrypted Data, 
 
 public:
     NTRUencrypt(int strength);
@@ -57,8 +57,8 @@ public:
     std::string generatePublicKey(std::string privateKey);  // Public Key Generator
     std::string generatePrivateKey(int seed);               // Private Key Generator - private key is fPolynomial with gPolynomial attached on the end of it
 
-    std::string encrypt(std::string privateKey, std::string receiverKey, std::string data);     // Uses string encryption, translates the resultPolynomial into hexadecimal
-    std::string decrypt(std::string privateKey, std::string receiverKey, std::string data);     // Uses string decryption, translates data from hexidecimal into a string array
+    std::string encrypt(std::string receiverKey, std::string data);     // Uses string encryption, translates the resultPolynomial into hexadecimal
+    std::string decrypt(std::string privateKey, std::string data);      // Uses string decryption, translates data from hexidecimal into a string array
 };
 
 #endif
