c05936bebd399fb122e0c5ca077f6848aaf19ec1
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-06-24 23:35:50 +1000

    Added core functions to LZMA
    Added the core LZMA functions to the header file + dictionary size and tuple vector + some different public compression/decompression inputs/outputs (better usability).

--- algorithms/lzma-compression/LZMA-Compression.h
+++ algorithms/lzma-compression/LZMA-Compression.h
@@ -8,12 +8,25 @@
 
 class LZMACompression {
 private:
+    size_t dictionarySize;
     std::vector<Position2D> dictionary;
-    
+    std::vector<std::pair<int, int>> tuples;
+
+    void initialiseDictionary(std::vector<char> data);                      // Initialises the dictionary with the given size
+    std::pair<int, int> findMatch(std::vector<char> sequence);              // Finds the longest match of the given sequence in the dictionary
+    void processUnmatchedBytes(std::vector<char> unmatchedBytes);           // Processes the unmatched bytes as literals
+    std::pair<int, int> encodeLength(int length);                           // Encodes the given length using LZMA's match length encoding
+    int decodeLength(std::pair<int, int> encodedLength);                    // Decodes the given length using LZMA'z match length encoding
+    std::vector<char> rangeEncode(std::vector<char> dataStream);            // Encodes the given data stream using range encoding
+    std::vector<char> rangeDecode(std::vector<char> encodedDataStream);     // Decodes the given data stream using range encoding
+
 public:
     LZMACompression();
 
-    std::string compress(std::string *decompressedData);
+    // Compresses or Decompresses the given data
+    std::vector<char> compress(std::vector<char> *inputData);
+    std::vector<char> decompress(std::vector<char> *compressedData);
+    std::string compress(std::string *inputData);
     std::string decompress(std::string *compressedData);
 };
 
