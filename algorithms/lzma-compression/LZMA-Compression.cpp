1f3c9f70971a4d8ebf1e1142961d62dbe9738dc5
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-06-25 20:34:25 +1000

    Added Core Function Implementations
    Added the implementation for the core functions, as well as updates to the header file with some secondary probability calculating functions being added for use in range encoding.

---
+++ algorithms/lzma-compression/LZMA-Compression.cpp
@@ -0,0 +1,124 @@
+#include "LZMA-Compression.h"
+
+
+void LZMACompression::initialiseDictionary(size_t size) {
+    dictionarySize = size;
+    dictionary.clear();
+    dictionary.resize(size, 0);
+}
+
+std::pair<int, int> LZMACompression::findMatch(std::vector<char> sequence) {
+    int matchingDistance = -1;
+    int matchingLength = -1;
+
+    // Check every position in the dictionary
+    for(int i = 0; i < dictionary.size(); i++) {
+        int length = 0;
+
+        // Compute the length of the match
+        while(length < sequence.size() && dictionary[(i + length) % dictionary.size()] == sequence[length])
+            length++;
+
+        if(length > matchingLength) {
+            matchingDistance = i;
+            matchingLength = length;
+        }
+    }
+
+    return std::make_pair(matchingDistance, matchingLength);
+}
+
+std::vector<char> LZMACompression::processUnmatchedBytes(std::vector<char> unmatchedBytes) {
+    std::vector<char> output;
+    std::map<char, float> probabilities;
+
+    for(char c : unmatchedBytes)    // Count occurence of each byte
+        probabilities[c]++;
+    
+    for(auto& p : probabilities)    // Convert counts to probabilities
+        p.second /= unmatchedBytes.size();
+
+    // Perform range encoding on the unmatched bytes: here, it is simplified to just scaling the character's ASCII value by probability
+    for(char c : unmatchedBytes)    // Will be changed out for a more robust range encoding function in the future
+        output.push_back(static_cast<char>(c * probabilities[c]));
+    
+    return output;
+}
+
+std::pair<int, int> LZMACompression::encodeLength(int length) {
+    return std::make_pair(length / 2, length % 2);  // Simple length encoding for now
+}
+
+int LZMACompression::decodeLength(std::pair<int, int> encodedLength) {
+    return encodedLength.first / 2;
+}
+
+std::vector<char> LZMACompression::rangeEncode(std::vector<char> dataStream) {
+    std::vector<char> output;
+    unsigned int low = 0;
+    unsigned int range = static_cast<unsigned int>(-1);
+
+    std::map<char, float> probabilities = calculateSymbolProbabilities(dataStream);     // Get the probability of each symbol
+
+    for(char symbol : dataStream) {
+        // Calculate the range for the symbol based on the probabilities
+        unsigned int scale = range;
+        unsigned int high = low + static_cast<unsigned int>(scale * probabilities[symbol]);
+        range = high - low;
+
+        // Update the low value based on the cumulative probability of the symbols
+        float cumulativeProbability = 0;
+        for(auto& p : probabilities) {
+            if(p.first == symbol)
+                break;
+
+            cumulativeProbability += p.second;
+        }
+
+        low += static_cast<unsigned int>(scale * cumulativeProbability);
+
+        // STILL TO IMPLEMENT HERE: Handing carrying, rescaling and output bits here
+    }
+
+    return output;  // STILL TO IMPLEMNT: Output should be the binary representation of the encoded range - smalles number that falls into the final range
+}
+
+std::vector<char> LZMACompression::rangeDecode(std::vector<char> encodedDataStream) {
+    std::vector<char> output;
+    unsigned int low = 0;
+    unsigned int range = static_cast<unsigned int>(-1);
+    unsigned int code = 0;
+
+    std::map<char, float> probabilities = getProbabilityDistribution(encodedDataStream);
+
+    // Decode each symbol in the encodedDataStream
+    for(size_t i = 0; i < encodedDataStream.size(); i++) {
+        unsigned int scale = range;     // Calculate the total scale of the probabilities
+
+        // Determine which range the code falls into:
+        for(auto&p : probabilities) {
+            unsigned int high = low + static_cast<unsigned int>(scale * p.second);
+
+            if(code < high) {
+                output.push_back(p.first);
+                range - high - low;
+
+                // Remove the contribution of all previous symbols from low
+                float cumulativeProbability = 0;
+                for(auto& prevP : probabilities) {
+                    if(prevP.first == p.first)
+                        break;
+                    
+                    cumulativeProbability += prevP.second;
+                }
+
+                low += static_cast<unsigned int>(scale * cumulativeProbability);
+                break;
+            }
+        }
+
+        // STILL TO IMPLEMENT HERE: Handing carrying, rescaling and output bits here
+    }
+
+    return output;
+}
--- algorithms/lzma-compression/LZMA-Compression.h
+++ algorithms/lzma-compression/LZMA-Compression.h
@@ -1,6 +1,9 @@
 #ifndef LZMACOMPRESSION_H
 #define LZMACOMPRESSION_H
 
+#include <map>
+#include <cmath>
+#include <deque>
 #include <string>
 #include <vector>
 #include "Position.h"
@@ -12,13 +15,15 @@ private:
     std::vector<Position2D> dictionary;
     std::vector<std::pair<int, int>> tuples;
 
-    void initialiseDictionary(std::vector<char> data);                      // Initialises the dictionary with the given size
-    std::pair<int, int> findMatch(std::vector<char> sequence);              // Finds the longest match of the given sequence in the dictionary
-    void processUnmatchedBytes(std::vector<char> unmatchedBytes);           // Processes the unmatched bytes as literals
-    std::pair<int, int> encodeLength(int length);                           // Encodes the given length using LZMA's match length encoding
-    int decodeLength(std::pair<int, int> encodedLength);                    // Decodes the given length using LZMA'z match length encoding
-    std::vector<char> rangeEncode(std::vector<char> dataStream);            // Encodes the given data stream using range encoding
-    std::vector<char> rangeDecode(std::vector<char> encodedDataStream);     // Decodes the given data stream using range encoding
+    void initialiseDictionary(size_t size);                                                 // Initialises the dictionary with the given size
+    std::pair<int, int> findMatch(std::vector<char> sequence);                              // Finds the longest match of the given sequence in the dictionary
+    std::vector<char> processUnmatchedBytes(std::vector<char> unmatchedBytes);              // Processes the unmatched bytes as literals
+    std::pair<int, int> encodeLength(int length);                                           // Encodes the given length using LZMA's match length encoding
+    int decodeLength(std::pair<int, int> encodedLength);                                    // Decodes the given length using LZMA'z match length encoding
+    std::map<char, float> calculateSymbolProbabilities(std::vector<char> dataStream);       // Calculates the symbol probabilities from the data stream
+    std::map<char, float> getProbabilityDistribution(std::vector<char> encodedDataStream);  // Gets the symbol probabilities from the encoded data stream
+    std::vector<char> rangeEncode(std::vector<char> dataStream);                            // Encodes the given data stream using range encoding
+    std::vector<char> rangeDecode(std::vector<char> encodedDataStream);                     // Decodes the given data stream using range encoding
 
 public:
     LZMACompression();
