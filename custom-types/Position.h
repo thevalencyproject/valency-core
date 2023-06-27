8625a83f883b202353d9c98b4cea26485435323d
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-06-27 22:11:45 +1000

    Added 4D and 5D Position
    Added structures for 4D and 5D objects - these (like the previous 2 structures) take any variable type.

--- custom-types/Position.h
+++ custom-types/Position.h
@@ -31,4 +31,40 @@ struct Position3D {
 	};
 };
 
+template<typename T>
+struct Position4D {
+	T x;
+	T y;
+	T z;
+	T i;
+
+	Position4D() {};
+
+	Position4D(T posX, T posY, T posZ, T posI) {
+		x = posX;
+		y = posY;
+		z = posZ;
+		i = posI;
+	};
+};
+
+template<typename T>
+struct Position5D {
+	T x;
+	T y;
+	T z;
+	T i;
+	T j;
+
+	Position5D() {};
+
+	Position5D(T posX, T posY, T posZ, T posI, T posJ) {
+		x = posX;
+		y = posY;
+		z = posZ;
+		i = posI;
+		j = posJ;
+	};
+};
+
 #endif
