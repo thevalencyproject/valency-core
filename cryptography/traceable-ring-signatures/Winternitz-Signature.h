0afe38335e9084b442558dd29012b2c6c8e78a4f
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-08-16 20:53:01 +1000

    Added Copyright Notice to Cryptography

--- cryptography/traceable-ring-signatures/Winternitz-Signature.h
+++ cryptography/traceable-ring-signatures/Winternitz-Signature.h
@@ -1,3 +1,6 @@
+/* This file is a part of The Valency Project which is released under the GNU Lesser General Public License v2.1
+   Go to https://github.com/thevalencyproject/valency-core/blob/main/LICENSE for further license details.        */
+
 #ifndef WINTERNITZ_SIGNATURE_H
 #define WINTERNITZ_SIGNATURE_H
 
